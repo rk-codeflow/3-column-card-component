82df35d6f982b03fe4c9eba596854a6cf9331f5f
RajKiran <rjkeeran@gmail.com>
2022-08-16 19:56:48 +0545

    image added

--- src/data.js
+++ src/data.js
@@ -3,28 +3,20 @@ export default [
         id: '1',
         imgUrl: 'https://raw.githubusercontent.com/rk-codeflow/3-column-card-component/4715ce1bab5e0a00f6392c0a17b0ac2ff10eaebd/images/icon-sedans.svg',
         title: 'sedans',
-        description: 'Choose a sedan for its affordability and excellent  fuel economy. Ideal for  cruising in the city or on your next road trip.',
-        // className: 'card-1',
-        // css: { background: 'tomato'}
+        description: 'Choose a sedan for its affordability and excellent  fuel economy. Ideal for  cruising in the city or on your next road trip.'
     },
 
     {
         id: '2',
         imgUrl: 'https://raw.githubusercontent.com/rk-codeflow/3-column-card-component/4715ce1bab5e0a00f6392c0a17b0ac2ff10eaebd/images/icon-suvs.svg',
         title: 'suvs',
-        description: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.',
-        // className: 'card-2',
-        // css: { background: 'purple'}
-
+        description: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.'
     },
 
     {
         id: '3',
         imgUrl: 'https://raw.githubusercontent.com/rk-codeflow/3-column-card-component/4715ce1bab5e0a00f6392c0a17b0ac2ff10eaebd/images/icon-luxury.svg',
         title: 'luxury',
-        description: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.',
-        // className: 'card-3',
-        // css: { background: 'pink'}
-
+        description: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.'
     }
 ]
