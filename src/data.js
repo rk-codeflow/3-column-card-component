f139e80f7a0a214b96fb4c7576e33af4dc76e813
RajKiran <rjkeeran@gmail.com>
2022-08-16 21:34:56 +0545

    image url updated

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 export default [
     {
         id: '1',
-        imgUrl: 'https://raw.githubusercontent.com/rk-codeflow/3-column-card-component/4715ce1bab5e0a00f6392c0a17b0ac2ff10eaebd/images/icon-sedans.svg',
-        title: 'sedans tttttttt',
+        imgUrl: 'https://raw.githubusercontent.com/rk-codeflow/3-column-card-component/4c21015ba7310e700e55f70a9a5ba4054777b758/public/icon-sedans.svg',
+        title: 'sedans',
         description: 'Choose a sedan for its affordability and excellent  fuel economy. Ideal for  cruising in the city or on your next road trip.'
     },
 
     {
         id: '2',
-        imgUrl: 'https://raw.githubusercontent.com/rk-codeflow/3-column-card-component/4715ce1bab5e0a00f6392c0a17b0ac2ff10eaebd/images/icon-suvs.svg',
+        imgUrl: 'https://raw.githubusercontent.com/rk-codeflow/3-column-card-component/4c21015ba7310e700e55f70a9a5ba4054777b758/public/icon-suvs.svg',
         title: 'suvs',
         description: 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.'
     },
 
     {
         id: '3',
-        imgUrl: 'https://raw.githubusercontent.com/rk-codeflow/3-column-card-component/4715ce1bab5e0a00f6392c0a17b0ac2ff10eaebd/images/icon-luxury.svg',
+        imgUrl: 'https://raw.githubusercontent.com/rk-codeflow/3-column-card-component/4c21015ba7310e700e55f70a9a5ba4054777b758/public/icon-luxury.svg',
         title: 'luxury',
         description: 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.'
     }
